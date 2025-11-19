77cdf7084a24f5e430e7883b58860ac196ae7e4a
Darckcrazy <oleksandrkozel2024@gmail.com>
2025-11-19 11:52:26 +0100

    Adds spring
    Aggiunta dei file java e spring

---
+++ Animeverse_JAVA/src/main/java/com/example/Animeverse_JAVA/AnimeverseJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.Animeverse_JAVA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimeverseJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimeverseJavaApplication.class, args);
+	}
+
+}
---
+++ Animeverse_JAVA/src/test/java/com/example/Animeverse_JAVA/AnimeverseJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Animeverse_JAVA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeverseJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
